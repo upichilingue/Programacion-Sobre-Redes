691c7ca9b3d995b51b116be530634412b789e5b1
upichilingue <118293956+upichilingue@users.noreply.github.com>
2023-08-25 12:05:06 -0300

    Add files via upload

---
+++ WindowsService1/WindowsService1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Crear una instancia del servicio
+            IncrementService service = new IncrementService();
+
+            // Ejecutar el servicio
+            ServiceBase.Run(service);
+
+        }
+
+    }
+}
---
+++ WindowsService1/WindowsService1/IncrementService.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+
+namespace WindowsService1
+{
+    public class IncrementService : ServiceBase
+    {
+
+        private Timer timer;
+        private int counter;
+
+        public IncrementService()
+        {
+
+            this.ServiceName = "IncrementService";
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            // Inicializar el contador
+            counter = 0;
+
+            // Crear un archivo txt si no existe
+            string path = @"E:\Increment.txt";
+            if (!File.Exists(path))
+            {
+
+                File.Create(path).Close();
+
+            }
+
+            // Inicializar el timer con un intervalo de 1 segundo
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(OnTimer);
+            timer.Start();
+
+        }
+
+        protected override void OnStop()
+        {
+
+            // Detener el timer
+            timer.Stop();
+
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+
+            // Incrementar el contador 
+            counter ++;
+
+            // Escribir el contador en el archivo txt
+            string path = @"E:\Increment.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+
+                sw.WriteLine(counter);
+
+            }
+
+        }
+
+    }
+}
