76760c7fbe99222fa0f0d162e0dc8b56ab8fd42e
upichilingue <118293956+upichilingue@users.noreply.github.com>
2023-11-07 11:22:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_SoapUI
+{
+    internal class Program
+    {
+
+        Soap1.TipoCambio client = new Soap1.TipoCambio();
+
+        static void Main(string[] args)
+        {
+
+            Soap1.TipoCambio client = new Soap1.TipoCambio();
+
+            var respuesta = client.TipoCambioDia();
+
+            Console.WriteLine(respuesta.CambioDolar.First().fecha);
+            Console.WriteLine(respuesta.CambioDolar.First().referencia);
+            Console.ReadLine();
+
+        }
+    }
+}
